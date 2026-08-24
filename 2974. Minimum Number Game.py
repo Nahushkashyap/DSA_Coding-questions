a6508ce207b7133deb5fe8c7261578f96d71162a
Nahush kashyap <nahushkashyap@MacBook-Air.local>
2026-08-25 00:19:55 +0530

    moved files to separate directory

--- 2974. Minimum Number Game.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def numberGame(self, nums: List[int]) -> List[int]:
-        ans = [0] * len(nums)
-        nums.sort(reverse=True)
-        for i in range(0, len(nums) - 1, 2):
-            ans[i + 1] = nums.pop()
-            ans[i] = nums.pop()
-        return ans
